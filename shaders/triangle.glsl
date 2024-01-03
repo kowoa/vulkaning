2a7122111db43cd14dff01cee940b429de723dbd
kowoa <koadahoakodah@gmail.com>
2024-01-03 13:59:24 -0500

    Render rgb triangle

--- shaders/triangle.glsl
+++ shaders/triangle.glsl
@@ -2,6 +2,8 @@
 
 #version 450
 
+layout (location = 0) out vec3 outColor;
+
 void main() {
     const vec3 positions[3] = vec3[3](
         vec3(1.f, 1.f, 0.0f),
@@ -9,7 +11,14 @@ void main() {
         vec3(0.f, -1.f, 0.0f)
     );
 
+    const vec3 colors[3] = vec3[3](
+        vec3(1.0f, 0.0f, 0.0f),
+        vec3(0.0f, 1.0f, 0.0f),
+        vec3(0.0f, 0.0f, 1.0f)
+    );
+
     gl_Position = vec4(positions[gl_VertexIndex], 1.0f);
+    outColor = colors[gl_VertexIndex];
 }
 
 
@@ -17,8 +26,9 @@ void main() {
 
 #version 450
 
+layout (location = 0) in vec3 inColor;
 layout (location = 0) out vec4 outFragColor;
 
 void main() {
-    outFragColor = vec4(1.f, 0.f, 0.f, 1.0f);
+    outFragColor = vec4(inColor, 1.0f);
 }
